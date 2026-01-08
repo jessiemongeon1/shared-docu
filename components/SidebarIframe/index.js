2c1b506f6739d826892380fb1ae41822558595e1
jessiemongeon1 <jessie.mongeon@mystenlabs.com>
2026-01-08 15:41:46 -0600

    Initial shared components

--- components/SidebarIframe/index.js
+++ components/SidebarIframe/index.js
@@ -1,7 +1,4 @@
-/*
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-*/
+
 
 import React, { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
